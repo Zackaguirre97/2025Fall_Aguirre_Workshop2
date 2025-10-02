410fd3950589b4d6a0fea2b14fc8ea2e448f3388
Zackaguirre <zackaguirre97@gmail.com>
2025-10-02 15:48:08 -0500

    Created the Book class. Added properties. Added checkOut & checkIn method shells.

---
+++ src/main/java/com/pluralsight/Book.java
@@ -0,0 +1,27 @@
+package com.pluralsight;
+
+public class Book {
+    // *** Properties ***
+    private int id;
+    private String isbn;
+    private String title;
+    private boolean isCheckedOut;
+    private String checkedOutTo;
+
+    // *** Constructor ***
+
+    // *** Getters/Setters ***
+
+    // *** Methods ***
+    // Check a book out.
+    public void checkOut(String name) {
+
+    }
+
+    // Check a book back in.
+    public void checkIn() {
+
+    }
+
+    // *** Overrides ***
+}
