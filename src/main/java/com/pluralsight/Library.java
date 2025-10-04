b96b48e58af8ea3c84316df6a95b79391e531589
Zackaguirre <zackaguirre97@gmail.com>
2025-10-03 21:47:02 -0500

    Creation of Library class. Properties: Array of Book objects ('library'). Constructor. Getter ('getLibrary'). Methods: showLibrary() displays all books in the 'library', showAvailableBooks() & showCheckedOutBooks() show the books in the array that are checked out or not- respectively. bookIdFound() checks the 'library' for a specified 'id', checkOutBook() & checkInBook() check books in library in or out- respectively given a specified name or name & id, and return true or false.

--- src/main/java/com/pluralsight/Library.java
+++ src/main/java/com/pluralsight/Library.java
@@ -24,19 +24,63 @@ public class Library {
 
     // * Display the available books
     public void showAvailableBooks() {
+        int counter = 0;
         for (Book book : library) {
-            if (book.isCheckedOut()) {
-                book.bookData();
+            if (!book.isCheckedOut()) {
+                book.availableBookData();
+                counter++;
             }
+        }
+        if (counter == 0) {
+            System.out.println("No books are currently checked available.");
         }
     }
 
     // * Display the checked out books
     public void showCheckedOutBooks() {
+        int counter = 0;
         for (Book book : library) {
-            if (!(book.isCheckedOut())) {
-                book.bookData();
+            if (book.isCheckedOut()) {
+                book.unavailableBookData();
+                counter++;
             }
         }
+        if (counter == 0) {
+            System.out.println("No books are currently checked out.");
+        }
+    }
+
+    // * Display the checked out books
+    public boolean bookIdFound(int id) {
+        boolean found = false;
+        for (Book book : library) {
+            if (book.getId() == id) {
+                found = true;
+                break;
+            }
+        }
+        return found;
+    }
+
+    // * Display the checked out books
+    public boolean checkOutBook(int id, String name) {
+        for (Book book : library) {
+            if (book.getId() == id) {
+                book.checkOut(name);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // * Display the checked in books
+    public boolean checkInBook(int id) {
+        for (Book book : library) {
+            if (book.getId() == id) {
+                book.checkIn();
+                return true;
+            }
+        }
+        return false;
     }
 }
